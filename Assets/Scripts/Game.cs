ce0149ebcb1aa9cc67361c9b9d0e0a62dfcc76ca
Daiya Oomine <farrasrahmatullah@gmail.com>
2017-11-27 15:52:06 +0700

    Inital Commit
    pls help guys

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public GameObject vertexku;
+    public GameObject edgeku;
+    public Vector2 spawnvalue;
+
+    public int score;
+    public int level;
+    public Text scoretext;
+    public Text leveltext;
+    private bool gameover;
+
+
+
+	// Use this for initialization
+	void Start () {
+        level = 1;
+        score = 0;
+        gameover = false;
+        UpdateLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void UpdateLevel()
+    {
+        leveltext.text = "Level: " + level;
+    }
+
+}
---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+	
+}
