5d24fb4b4ac2bcd51547a12cc88ddcabd8683f90
Daiya Oomine <farrasrahmatullah@gmail.com>
2017-11-30 20:07:31 +0700

    Dikit selesainya
    hehe

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
 using System;
+
 
 public class Game : MonoBehaviour {
 
@@ -11,34 +13,43 @@ public class Game : MonoBehaviour {
     public Vector2 spawnvalue;
     public Quaternion spawnrotation;
     public GameObject[] tombolwarna;
-    private ColorBlock theColor;
-    private Button theButton;
 
-    public int score;
+
+    private GameObject[] vertexnya;
+    private Color warnanya;
+    private Button theButton;
+    private float timeAmt = 10;
+    private float time;
+
+
+
+
     public int level;
     public Text timertext;
     public Text leveltext;
-    private bool gameover;
-
+   // private bool gameover;
 
 
 	// Use this for initialization
 	void Start () {
         level = 1;
-        score = 0;
+        time = timeAmt;
         CreateColor();
 
-        gameover = false;
+        
         spawnvalue = new Vector2(0, 0);
         spawnrotation = new Quaternion(0, 0, 0, 0);
 
-        vertexku.transform.Find("Numbering").GetComponent<TextMesh>().text = "9";
-        Instantiate(vertexku, spawnvalue, spawnrotation);
+        float r = UnityEngine.Random.Range(0.0f, 1.0f);
+        float g = UnityEngine.Random.Range(0.0f, 1.0f);
+        float b = UnityEngine.Random.Range(0.0f, 1.0f);
+        Color baru = new Color(r, g, b);
+        warnanya= Color.white;
+        vertexnya = new GameObject[16];
 
 
-        spawnvalue = new Vector2(2, 2);
-        vertexku.transform.Find("Numbering").GetComponent<TextMesh>().text = "19";
-        Instantiate(vertexku, spawnvalue, spawnrotation);
+        vertexnya[0] = Instantiate(vertexku, spawnvalue, spawnrotation);
+        vertexnya[0].SendMessage("SetColor",baru);
 
         UpdateLevel();
 	}
@@ -56,8 +67,26 @@ public class Game : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-		
+    void Update ()
+    {
+        UpdateTime();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics2D.Raycast(ray.origin, ray.direction))
+            {
+                for (int i = 0; i < vertexnya.Length; i++)
+                {
+                    if (hit.collider.gameObject == vertexnya[i])
+                    {
+                        vertexnya[i].SendMessage("SetColor", warnanya);
+                    }
+                }
+            }
+                
+        }
 	}
 
 
@@ -65,5 +94,21 @@ public class Game : MonoBehaviour {
     {
         leveltext.text = "Level: " + level;
     }
+
+    public void UpdateTime()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            timertext.text = "Time " + time.ToString("F");
+        }
+        else SceneManager.LoadScene("Game Over");
+    }
+
+    public void Set_Warna(int diklik)
+    {
+        warnanya=tombolwarna[diklik].GetComponent<Image>().color;
+    }
+
 
 }
---
+++ Assets/Scripts/Vertex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Vertex : MonoBehaviour {
+
+    private SpriteRenderer mysprite;
+
+    Vertex(Color colors)
+    {
+        mysprite.color= colors;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        //mysprite = GetComponent<SpriteRenderer>();
+        //mysprite.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void SetColor(Color colors)
+    {
+        //mysprite.color = Color.yellow;
+        this.GetComponent<SpriteRenderer>().color = colors;
+    }
+
+}
